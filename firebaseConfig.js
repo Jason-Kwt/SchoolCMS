9ba0d122f495f8c74e4d519a6ba74788a95cc9fb
Jason Kong <jasonxkong@icloud.com>
2023-02-15 22:22:33 +0800

    First commit

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAG3WRAqz_x4-vINctuzlxpbJh3aUOclwg',
+  authDomain: 'schoolcms-547e5.firebaseapp.com',
+  projectId: 'schoolcms-547e5',
+  storageBucket: 'schoolcms-547e5.appspot.com',
+  messagingSenderId: '312683273095',
+  appId: '1:312683273095:web:c17eeb171012ced33f5122',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
